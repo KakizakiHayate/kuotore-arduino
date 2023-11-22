42acb8dd3569962f3ae2dddd9122591feb8b5834
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-22 11:13:16 +0900

    距離を1秒間に一回取得するようにした

--- kuotore.ino
+++ kuotore.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <M5StickCPlus.h>
+#include <Wire.h>
+#include <VL53L0X.h>
 
+VL53L0X sensor;
+TFT_eSprite img = TFT_eSprite(&M5.Lcd);
+
+void setup() {
+  M5.begin();
+  Wire.begin(0, 26);
+
+  img.createSprite(160, 80);
+  img.fillSprite(BLACK);
+  img.setTextColor(WHITE);
+  img.setTextSize(2);
+
+  sensor.setTimeout(500);
+  if (!sensor.init()) {
+    img.setCursor(10, 10);
+    img.print("Failed");
+    img.pushSprite(0,0);
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
+  sensor.startContinuous(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  uint16_t distance = sensor.readRangeContinuousMillimeters();
+  Serial.print(distance);
+  if (sensor.timeoutOccurred()) {
+    Serial.print("TIMEOUT");
+  }
+  Serial.println();
+  img.fillSprite(BLACK);
+  img.setCursor(0,0);
+  img.print(distance);
+  img.print("mm");
+  img.pushSprite(0,0);
+  delay(500);
 }
